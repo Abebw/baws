6a4be909ad93c4e2783c7031d0e62c1d67eca82b
abe <abe@abebw.com>
2009-12-10 14:53:11 -0500

    added wasd movement

--- baws.py
+++ baws.py
@@ -21,26 +21,43 @@ screen = pygame.display.set_mode((600,400))
 clock = pygame.time.Clock()
 running=True
 
-
+def add(x,y):
+    return [x[0]+y[0], x[1]+y[1]]
 class Player (pygame.sprite.Sprite):
     def __init__(self):
         self.image = pygame.Surface([30,15])
         self.image.fill([100,100,100])
         self.rect = self.image.get_rect()
         self.rect.center = [100,100]
-        self.direction = [1,0]
+        self.direction = [0,0]
     def update(self):
         self.rect = self.rect.move(self.direction)
 
 def processevent(e):
     global running
-    print e
+    global p
     if e.type == pygame.QUIT:
         running = False
     if e.type == pygame.KEYDOWN:
-        print "keydown"
-        if e.key == "pygame.key.K_w<this is wrong>":
-            print "w down"
+        if e.key == pygame.K_w:
+            p.direction = add(p.direction,[0,-2])
+        elif e.key == pygame.K_s:
+            p.direction = add(p.direction,[0,2])
+        elif e.key == pygame.K_a:
+            p.direction = add(p.direction,[-2,0])
+        elif e.key == pygame.K_d:
+            p.direction = add(p.direction,[2,0])
+    if e.type == pygame.KEYUP:
+        if e.key == pygame.K_w:
+            p.direction = add(p.direction,[0,2]) 
+        elif e.key == pygame.K_s:
+            p.direction = add(p.direction,[0,-2])
+        elif e.key == pygame.K_a:
+            p.direction = add(p.direction,[2,0])
+        elif e.key == pygame.K_d:
+            p.direction = add(p.direction,[-2,0])
+                
+            
 def draw():
     global screen
     global p
